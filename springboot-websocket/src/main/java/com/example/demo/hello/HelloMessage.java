4485273c423dca3329e0aa86e0554d1a3ffe12f7
alibeibei <1939612669@qq.com>
2017-12-06 10:10:49 +0800

    websocket

---
+++ springboot-websocket/src/main/java/com/example/demo/hello/HelloMessage.java
@@ -0,0 +1,24 @@
+package com.example.demo.hello;
+
+/**
+ * Created by alibeibei on 2017/12/6.
+ */
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
