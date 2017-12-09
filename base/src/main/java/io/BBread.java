12ffcaa3807900df33a0d6f4554180e3193478d6
alibeibei <1939612669@qq.com>
2017-12-10 00:18:49 +0800

    config

---
+++ base/src/main/java/io/BBread.java
@@ -0,0 +1,12 @@
+package io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BBread {
+    public static void main(String[] args) {
+        char c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
---
+++ springboot-configuration/src/main/java/com/example/springbootconfiguration/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.springbootconfiguration.controller;
+
+import com.example.springbootconfiguration.configuration.MyConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class HelloController {
+    @Autowired
+    private MyConfig myConfig;
+
+    @RequestMapping("/config")
+    public Object getConfig() {
+        return myConfig.getServers();
+    }
+}
